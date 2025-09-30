df5cdc3ecb4cb6d51bd7c14c432244050bd804a3
compscibear <cadelewis2024@gmail.com>
2025-09-30 15:17:02 -0700

    finished v1 of website

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./Hero";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      {/* Example next section - replace with your real content */}
+    </div>
+  );
+}
+
+export default App;
